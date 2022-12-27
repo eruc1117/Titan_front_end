7370862636d98e45630a531db5953af5bf199cc5
eruc <eruc101010@gmail.com>
2022-12-27 23:31:14 +0800

    feat: connect the login API and save the token in localstorage

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,82 +1,114 @@
+<script setup>
+import { ref } from "vue";
+
+const basicUrl = "http://localhost:3000";
+
+const account = ref("");
+const password = ref("");
+
+async function login() {
+  fetch(`${basicUrl}/account/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      account: account.value,
+      password: password.value,
+    }),
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      localStorage.setItem("token", response.message.token);
+    })
+    .catch((error) => console.error("Error:", error));
+}
+</script>
+
 <template>
   <div class="login">
-    <div class="inputDiv"> 
-    <label>Account</label>
-    <input type="text">
+    <div class="inputDiv">
+      <label>Account</label>
+      <input
+        type="text"
+        :value="account"
+        v-on:input="(event) => (account = event.target.value)"
+      />
     </div>
     <div class="inputDiv">
-    <label>Password</label>  
-    <input type="password">
+      <label>Password</label>
+      <input
+        type="text"
+        :value="password"
+        v-on:input="(event) => (password = event.target.value)"
+      />
     </div>
-      <button class="loginButton">Login</button>
-    <a href="/home" class="resetLink" >Forget password</a>
+    <button class="loginButton" @click="login">Login</button>
+    <a href="/home" class="resetLink">Forget password</a>
     <router-link :to="{ name: 'home' }">Home</router-link>
     <router-link :to="{ name: 'admin' }">Admin</router-link>
   </div>
 </template>
 
 <style scoped>
-  .login {
-    width: 30%;
-    margin: auto;
-    margin-top: 5%;
-    background-color: rgb(175, 212, 241);
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-  .login > * {
-    width: 70%;
-  }
+.login {
+  width: 30%;
+  margin: auto;
+  margin-top: 5%;
+  background-color: rgb(175, 212, 241);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.login > * {
+  width: 70%;
+}
 
-  .inputDiv {
-    margin-top: 5%;
-    display: flex;
-    flex-direction: column;
-  }
+.inputDiv {
+  margin-top: 5%;
+  display: flex;
+  flex-direction: column;
+}
 
-  .inputDiv > label {
-    font-size: 25px;
-  }
+.inputDiv > label {
+  font-size: 25px;
+}
 
-  .inputDiv > input {
-    margin-top: 3%;
-    height: 30px;
-    font-size: 20px;
-    padding: 10px;
-  }
+.inputDiv > input {
+  margin-top: 3%;
+  height: 30px;
+  font-size: 20px;
+  padding: 10px;
+}
 
-  .inputDiv > input[type=text]:focus {
-    background-color: rgb(253, 161, 92);
-  }
+.inputDiv > input[type="text"]:focus {
+  background-color: rgb(253, 161, 92);
+}
 
-  .inputDiv > input[type=password]:focus {
-    background-color: rgb(248, 139, 123);
-  }
+.inputDiv > input[type="password"]:focus {
+  background-color: rgb(248, 139, 123);
+}
 
-  .loginButton {
-    margin-top: 5%;
-  }
+.loginButton {
+  margin-top: 5%;
+}
 
-  .loginButton {
-    width: 30%;
-    height: 30px;
-    background-color: blanchedalmond;
-    color: brown;
-  }
+.loginButton {
+  width: 30%;
+  height: 30px;
+  background-color: blanchedalmond;
+  color: brown;
+}
 
-  .loginButton:hover {
-    background-color: black;
-  }
+.loginButton:hover {
+  background-color: black;
+}
 
-  .resetLink {
-    margin-top: 5%;
-    margin-bottom: 3%;
-    text-align: center;
-  }
+.resetLink {
+  margin-top: 5%;
+  margin-bottom: 3%;
+  text-align: center;
+}
 
-  .resetLink:hover {
-    color: rgb(255, 0, 0);
-  }
-
+.resetLink:hover {
+  color: rgb(255, 0, 0);
+}
 </style>
