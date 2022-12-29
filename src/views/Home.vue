7f3bd181d3b88b4817e4dc957af57b8d73d3c497
eruc <eruc101010@gmail.com>
2022-12-29 23:34:46 +0800

    feat: chaining CheckIn API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,12 @@
 <script setup>
 import { ref } from "vue";
 import QRCodeVue3 from "qrcode-vue3";
+import { useGeolocation } from "@vueuse/core";
+const { coords, locatedAt, error, resume, pause } = useGeolocation();
 const basicUrl = "http://localhost:3000";
 const jwtToken = localStorage.getItem("token");
+const startTime = ref("--:--:--");
+const endTime = ref("--:--:--");
 let qrCodeUrl = "";
 const userId = ref("");
 const qrcodeValue = ref(`${basicUrl}/checkIn/qrCode/${qrCodeUrl}/${1}`);
@@ -21,22 +25,49 @@ async function preQrcode() {
     })
     .catch((error) => console.error("Error:", error));
 }
+
+async function checkIn() {
+  fetch(`${basicUrl}/checkIn/check`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${jwtToken}`,
+    },
+    body: JSON.stringify({
+      userId: 1,
+      location: {
+        latitude: coords.value.latitude,
+        longitude: coords.value.longitude,
+      },
+    }),
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      if (response.startTime !== "Invalid date") {
+        startTime.value = response.startTime.split(" ")[1];
+      }
+      if (response.endTime !== "Invalid date") {
+        endTime.value = response.endTime.split(" ")[1];
+      }
+    })
+    .catch((error) => console.error("Error:", error));
+}
 </script>
 
 <template>
   <div class="checkDiv">
-    <button class="checkButton">簽到</button>
+    <button class="checkButton" @click="checkIn">簽到</button>
     <div>
       <QRCodeVue3 :width="100" :height="100" :value="qrcodeValue" />
     </div>
     <button class="qrCodeButton" @click="preQrcode">QR code</button>
     <div class="timePoint">
       <p>上班時間</p>
-      <p>10:00</p>
+      <p>{{ startTime }}</p>
     </div>
     <div class="timePoint">
       <p>下班時間</p>
-      <p>19:00</p>
+      <p>{{ endTime }}</p>
     </div>
   </div>
 </template>
