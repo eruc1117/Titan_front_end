d5874e9c349394a72f5c9f1551975298a5141069
eruc <eruc101010@gmail.com>
2022-12-29 20:31:07 +0800

    feat: concatenate qrCode API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,35 @@
+<script setup>
+import { ref } from "vue";
+import QRCodeVue3 from "qrcode-vue3";
+const basicUrl = "http://localhost:3000";
+const jwtToken = localStorage.getItem("token");
+let qrCodeUrl = "";
+const userId = ref("");
+const qrcodeValue = ref(`${basicUrl}/checkIn/qrCode/${qrCodeUrl}/${1}`);
+async function preQrcode() {
+  fetch(`${basicUrl}/checkIn/preQrCode/${1}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${jwtToken}`,
+    },
+  })
+    .then((response) => response.json())
+    .then((response) => {
+      qrCodeUrl = response.message.urlVerCode;
+      qrcodeValue.value = `${basicUrl}/checkIn/qrCode/${qrCodeUrl}/${1}`;
+    })
+    .catch((error) => console.error("Error:", error));
+}
+</script>
+
 <template>
   <div class="checkDiv">
     <button class="checkButton">簽到</button>
+    <div>
+      <QRCodeVue3 :width="100" :height="100" :value="qrcodeValue" />
+    </div>
+    <button class="qrCodeButton" @click="preQrcode">QR code</button>
     <div class="timePoint">
       <p>上班時間</p>
       <p>10:00</p>
@@ -29,6 +58,14 @@
   height: 100px;
   border-radius: 100%;
 }
+
+.qrCodeButton {
+  width: 50px;
+  margin-top: 4%;
+  height: 50px;
+  border-radius: 100%;
+}
+
 .timePoint {
   display: flex;
   flex-direction: row;
